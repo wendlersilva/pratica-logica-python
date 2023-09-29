c2f65cd9a6bcf35c3e6972d15616bd16abbe5d90
wendler <wendlerconstancio@gmail.com>
2023-09-28 21:34:17 -0300

    Primeiro código em Python

---
+++ Primeiros Passos.py
@@ -0,0 +1,15 @@
+nome = "Wendler Silva"
+print(nome)
+
+nome = 27
+print(nome)
+
+nome = 25 + 12
+print(nome)
+
+nomeUsuario = "Wendler"; sobreNomeUsuario = "Silva"
+print(nomeUsuario + sobreNomeUsuario)
+print(nomeUsuario, sobreNomeUsuario)
+
+print("Nome:", nomeUsuario)
+print("Sobrenome:", sobreNomeUsuario)
