1b7523c1067b269d8e3aa0a558e1a84d90cda673
wendler <wendlerconstancio@gmail.com>
2023-10-01 23:46:13 -0300

    Tipos de Operadores

---
+++ Operadores Aritméticos.py
@@ -0,0 +1,26 @@
+numero1 = 10
+numero2 = 5
+
+adicao = numero1 + numero2
+print("Resultado adição:", adicao)
+
+subtracao = numero1 - numero2
+print("Resultado subtração:", subtracao)
+
+print("Wendler" + " " + "Silva")
+
+multiplicacao = numero1 * numero2
+print("Resultado multiplicação:", multiplicacao)
+print(numero1 * "W")
+
+divisao = numero1 / numero2
+print("Resultado divisão:", divisao)
+
+divisaoInteira = numero1 // numero2
+print("Resultado divisão inteira:", divisaoInteira)
+
+modulo = numero1 % numero2
+print("Resultado módulo:", modulo)
+
+exponenciacao = numero1 ** numero2
+print("Resultado exponencição inteira:", exponenciacao)
