b2cc0be0001bc6463e07f8d7e3ff60113997240c
wendler <wendlerconstancio@gmail.com>
2023-10-03 23:32:40 -0300

    Operadores Relacionais

---
+++ Operadores Relacionais.py
@@ -0,0 +1,34 @@
+letra = "a"
+
+#Operador Igual
+if letra == "a":
+   print("A letra é igual a Letra A")
+
+#Operador Diferente De
+if letra != "a":
+    print("A letra é diferente de A")
+
+#Operador Maior que
+numero1 = 20
+numero2 = 15
+
+if numero1 > numero2:
+    print("O número 1 é maior do que o número 2")
+
+#Operador Menor que
+n3 = 2
+n4 = 8
+if n3 < n4:
+    print("O número 3 é menor do que o número 4")
+
+#Operador Maior ou igual que
+num1 = 50
+num2 = 20
+if num1 >= 20:
+    print("O num1 é maior do que 20")
+
+#Operador Menor ou igual que
+num3 = 20
+num4 = 100
+if num3 <= 100:
+    print("O num3 é menor do que 100")
