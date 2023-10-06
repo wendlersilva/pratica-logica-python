6f398189264fddf21d30cea62ae57ca1b7ca5b7b
wendler <wendlerconstancio@gmail.com>
2023-10-05 23:19:43 -0300

    Tipos de Operadores

---
+++ Condicional If Elif Else.py
@@ -0,0 +1,24 @@
+n1 = 55
+n2 = 50
+
+if n1 > n2:
+    print("n1 é maior do que n2")
+elif n1 == n2:
+    print("n1 e n2 são iguais")
+
+
+numero1 = 10
+numero2 = 20
+
+if numero1 > numero2:
+    print("numero1 é maior do que numero2")
+elif numero1 == numero2:
+    print("numero1 e numero2 são iguais")
+else:
+    print("Nenhuma das condições atende ao critério")
+
+
+num1 = 5
+num2 = 12
+
+print("num1 é maior") if num1 > num2 else print("num 2 é maior")
---
+++ Operadores Lógicos.py
@@ -0,0 +1,37 @@
+numero1 = 25
+numero2 = 21
+numero3 = 40
+
+if numero1 > numero2 and numero3 > numero1:
+    print("Ambas as condições são verdadeiras")
+
+
+nome = "Wendler"
+idade = 27
+
+if nome == "Wendler" and idade > 18:
+    print("Wendler é maior de idade")
+
+
+usuario = "Wendler"
+senha = 123
+
+if usuario == "Wendler" and senha == 123:
+    print("Usuário logado com sucesso")
+else:
+    print("Usúario ou senha inválidos")
+
+
+n1 = 10
+n2 = 15
+n3 = 20
+
+if n1 > n2 or n1 > 5:
+    print("Pelo menos uma das condições é verdadeiro")
+
+
+fruta = "Laranja"
+
+if fruta == "Maca" or fruta == "Laranja":
+    print("A fruta é Maça ou Laranja")
+
