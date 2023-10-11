fe33889e5006cdd9688da0f8bc86b0b8e1dc3969
wendler <wendlerconstancio@gmail.com>
2023-10-10 22:45:40 -0300

    Uso de Lista 1

---
+++ Listas Exemplo 1.py
@@ -0,0 +1,26 @@
+listaNome = ["Wendler", "Silva"]
+print(listaNome)
+print(len(listaNome)) #Len apresenta o tamanho da lista
+
+print("\n")
+
+lista1 = ["A", "B", "C"]
+lista2 = [1, 2, 3, 4]
+lista3 = [True, False, False]
+lista4 = ["Wendler", 27, False]
+
+print(lista1)
+print(lista2)
+print(lista3)
+print(lista4)
+
+print("\n")
+
+listaTeste = ["A", "Banana", "C", "Tigre"]
+print(listaTeste[0])
+print(listaTeste[-1])
+print(listaTeste[1:3])
+print(listaTeste[:3])
+print(listaTeste[2:])
+print(listaTeste[::2])
+print(listaTeste[::-1])
